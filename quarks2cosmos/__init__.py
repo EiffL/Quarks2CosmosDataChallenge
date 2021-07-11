07b1b5f3cac6517924fb2ad6ee2e3ce0cff5392a
EiffL <fr.eiffel@gmail.com>
2021-07-12 00:01:41 +0200

    adding dae tools

--- quarks2cosmos/__init__.py
+++ quarks2cosmos/__init__.py
@@ -1,1 +1,4 @@
 import quarks2cosmos.galjax as galjax
+import quarks2cosmos.datasets as datasets
+import quarks2cosmos.models as models
+import quarks2cosmos.lensing as lensing
---
+++ quarks2cosmos/models/__init__.py
@@ -0,0 +1,2 @@
+from quarks2cosmos.models.convdae import SmallUResNet
+from quarks2cosmos.models.normalization import SpectralNorm, SNParamsTree
